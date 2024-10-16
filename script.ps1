9b3e729209a585498fe001419f12893deeac28be
Ibon <www.ibonhoz@gmail.com>
2024-10-16 13:04:17 +0200

    Primero

---
+++ script.ps1
@@ -0,0 +1,4 @@
+﻿New-Item -Path "C:\" -Name "prueba" -ItemType "directory"
+Set-Location -Path C:\prueba
+New-Item -Path . -Name "hola.txt" -ItemType "file" -Value "Ibon Hoz"
+Copy-Item -Path "C:\prueba\hola.txt" -Destination "C:\prueba\hola.bak"
